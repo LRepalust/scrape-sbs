4e323ab1a01c286eff685722d2bd0af55eca6149
LRepalust <111300307+LRepalust@users.noreply.github.com>
2022-09-02 15:19:53 +0200

    changes in spider and items

--- sbs/sbs/spiders/items.py
+++ sbs/sbs/spiders/items.py
@@ -38,9 +38,39 @@ def end_date(value):
 
 
 def extend(value):
+    p1 = r"\d+\s\bmonth(s*)\b"
+    p2 = r"(exten)"
+    result1 = re.search(p1, value)
+    result2 = re.search(p2, value)
+    if result2:
+        if result1:
+            return result1.group()
+        elif '1 year' in value:
+            return '12 months'
+        elif '2023' and '2025' in value:
+            return '24 months'
+        elif '2024' and '2026' in value:
+            return '24 months'
+        elif '2027' and '2023' in value:
+            return '48 months'
+        else:
+            pass
 
 
-class SbsItem(scrapy.Item):
+def get_lot_number(value):
+    p = r"\b(?:LOT|Lot)\b\s\d+"
+    results = re.findall(p, value)
+    return results
+
+
+def get_lot_description(value):
+    p = r"\b(?:LOT|Lot)\b\s\d+"
+    result = re.search(p, value)
+    if result:
+        return re.split(p, value)
+
+
+class SbsOverview(scrapy.Item):
     code = scrapy.Field(input_processor=MapCompose(remove_reference), output_processor=TakeFirst())
     organisation = scrapy.Field(output_processor=TakeFirst())
     url = scrapy.Field(output_processor=TakeFirst())
@@ -50,3 +80,6 @@ class SbsItem(scrapy.Item):
     start_date = scrapy.Field(input_processor=MapCompose(start_date), output_processor=TakeFirst())
     end_date = scrapy.Field(input_processor=MapCompose(end_date), output_processor=TakeFirst())
     extension_options = scrapy.Field(input_processor=MapCompose(extend), output_processor=TakeFirst())
+    lot_number = scrapy.Field(input_processor=MapCompose(get_lot_number))
+    lot_description = scrapy.Field(input_processor=MapCompose(get_lot_description))
+    supplier_name = scrapy.Field()
