dc15272bb2eb43cb7e3820ba3f92151bef924833
Leon Repalust <111300307+LRepalust@users.noreply.github.com>
2022-09-15 17:34:14 +0200

    Add files via upload

--- sbs/sbs/spiders/sbs_spider.py
+++ sbs/sbs/spiders/sbs_spider.py
@@ -3,40 +3,34 @@ import scrapy
 from sbs.items import SbsItems, SbsItemLoader
 
 
-
 class SbsSpider(scrapy.Spider):
     name = 'sbs_spider'
     allowed_domains = ['sbs.nhs.uk']
     start_urls = ['https://sbs.nhs.uk/proc-framework-agreements-support']
 
     def parse(self, response):
-        category_urls = response.css('#maincontent .container + .a-panel.a-panel--list a.item__link::attr(href)').getall()
+        category_urls = response.css(
+            '#maincontent .container + .a-panel.a-panel--list a.item__link::attr(href)').getall()
         for category_url in category_urls:
             category_url = response.urljoin(category_url)
             yield scrapy.Request(url=category_url, callback=self.parse_services)
 
     def parse_services(self, response):
-        framework_urls = response.css(
-            'a.a_body__link::attr(href)').getall()
+        framework_urls = response.css('a.a_body__link::attr(href)').re(r'/fas.*')
         for framework_url in framework_urls:
-            if '/fas' in framework_url:
-                framework_url = response.urljoin(framework_url)
-                yield scrapy.Request(url=framework_url, callback=self.parse_items)
-            elif '/ica' in framework_url:
-                framework_url = response.urljoin(framework_url)
-                yield scrapy.Request(url=framework_url, callback=self.parse_items)
-            else:
-                pass
+            framework_url = response.urljoin(framework_url)
+            yield scrapy.Request(url=framework_url, callback=self.parse_items)
 
     def parse_items(self, response):
         loader = SbsItemLoader(item=SbsItems(), selector=response)
         loader.add_css('code', 'p>span::text')
         loader.add_value('organisation', 'sbs')
         loader.add_value('url', response.url)
-        loader.add_xpath('category', '/html/body/header/div[3]/div/ol/li[5]/a//text()')
+        loader.add_css('category', '.breadcrumb a::text',
+                       re=r'(Business Services|Health|Digital & IT|Construction & Estates)')
         loader.add_xpath('description', '/html/body/main/div/div[3]/div/p[2]//text()')
         loader.add_css('framework_title', 'h1.a-heading__title::text')
-        loader.add_css('start_date', 'p:nth-child(6)')
+        loader.add_css('start_date', 'p:nth-child(6)::text')
         loader.add_css('start_date', 'p:nth-child(7)::text')
         loader.add_css('start_date', 'p:nth-child(8)::text')
         loader.add_css('start_date', 'p:nth-child(9)::text')
