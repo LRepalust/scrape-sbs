ec8221bcb1030491e7253dee4b1ad41086ebf541
Leon Repalust <111300307+LRepalust@users.noreply.github.com>
2022-09-15 15:57:46 +0200

    Changes in spider.py

--- sbs/sbs/spiders/sbs_spider.py
+++ sbs/sbs/spiders/sbs_spider.py
@@ -1,8 +1,7 @@
 import re
-
 import scrapy
-from sbs.items import SbsItems
-from scrapy.loader import ItemLoader
+from sbs.items import SbsItems, SbsItemLoader
+
 
 
 class SbsSpider(scrapy.Spider):
@@ -11,10 +10,10 @@ class SbsSpider(scrapy.Spider):
     start_urls = ['https://sbs.nhs.uk/proc-framework-agreements-support']
 
     def parse(self, response):
-        urls = response.css('#maincontent .container + .a-panel.a-panel--list a.item__link::attr(href)').getall()
-        for url in urls:
-            url = response.urljoin(url)
-            yield scrapy.Request(url=url, callback=self.parse_services)
+        category_urls = response.css('#maincontent .container + .a-panel.a-panel--list a.item__link::attr(href)').getall()
+        for category_url in category_urls:
+            category_url = response.urljoin(category_url)
+            yield scrapy.Request(url=category_url, callback=self.parse_services)
 
     def parse_services(self, response):
         framework_urls = response.css(
@@ -30,35 +29,35 @@ class SbsSpider(scrapy.Spider):
                 pass
 
     def parse_items(self, response):
-        l = ItemLoader(item=SbsItems(), selector=response)
-        l.add_css('code', 'p>span::text')
-        l.add_value('organisation', 'sbs')
-        l.add_value('url', response.url)
-        l.add_xpath('category', '/html/body/header/div[3]/div/ol/li[5]/a//text()')
-        l.add_xpath('description', '/html/body/main/div/div[3]/div/p[2]//text()')
-        l.add_css('framework_title', 'h1.a-heading__title::text')
-        l.add_css('start_date', 'p:nth-child(6)::text')
-        l.add_css('start_date', 'p:nth-child(7)::text')
-        l.add_css('start_date', 'p:nth-child(8)::text')
-        l.add_css('start_date', 'p:nth-child(9)::text')
-        l.add_css('start_date', 'p:nth-child(11)::text')
-        l.add_css('start_date', 'p:nth-child(14)::text')
-        l.add_css('start_date', 'tr>td>p:nth-child(2)::text')
-        l.add_css('end_date', 'p:nth-child(6)::text')
-        l.add_css('end_date', 'p:nth-child(7)::text')
-        l.add_css('end_date', 'p:nth-child(8)::text')
-        l.add_css('end_date', 'p:nth-child(9)::text')
-        l.add_css('end_date', 'p:nth-child(11)::text')
-        l.add_css('end_date', 'p:nth-child(14)::text')
-        l.add_css('end_date', 'tr>td>p:nth-child(2)::text')
-        l.add_css('extension_options', 'p:nth-child(7)::text')
-        l.add_css('extension_options', 'p:nth-child(8)::text')
-        l.add_css('extension_options', 'p:nth-child(9)::text')
-        l.add_css('lot_number', 'span.procgreentext::text')
-        l.add_css('lot_description', 'span.procgreentext::text')
-        l.add_css('lot_number', '.p:nth-child(12)::text')
-        l.add_css('lot_description', 'p:nth-child(12)::text')
-        l.add_css('lot_number', 'p:nth-child(10)>a::text')
-        l.add_css('lot_description', 'p:nth-child(10)>a::text')
-        l.add_xpath('supplier_name', '//td/p/a//text()')
-        yield l.load_item()
+        loader = SbsItemLoader(item=SbsItems(), selector=response)
+        loader.add_css('code', 'p>span::text')
+        loader.add_value('organisation', 'sbs')
+        loader.add_value('url', response.url)
+        loader.add_xpath('category', '/html/body/header/div[3]/div/ol/li[5]/a//text()')
+        loader.add_xpath('description', '/html/body/main/div/div[3]/div/p[2]//text()')
+        loader.add_css('framework_title', 'h1.a-heading__title::text')
+        loader.add_css('start_date', 'p:nth-child(6)')
+        loader.add_css('start_date', 'p:nth-child(7)::text')
+        loader.add_css('start_date', 'p:nth-child(8)::text')
+        loader.add_css('start_date', 'p:nth-child(9)::text')
+        loader.add_css('start_date', 'p:nth-child(11)::text')
+        loader.add_css('start_date', 'p:nth-child(14)::text')
+        loader.add_css('start_date', 'tr>td>p:nth-child(2)::text')
+        loader.add_css('end_date', 'p:nth-child(6)::text')
+        loader.add_css('end_date', 'p:nth-child(7)::text')
+        loader.add_css('end_date', 'p:nth-child(8)::text')
+        loader.add_css('end_date', 'p:nth-child(9)::text')
+        loader.add_css('end_date', 'p:nth-child(11)::text')
+        loader.add_css('end_date', 'p:nth-child(14)::text')
+        loader.add_css('end_date', 'tr>td>p:nth-child(2)::text')
+        loader.add_css('extension_options', 'p:nth-child(7)::text')
+        loader.add_css('extension_options', 'p:nth-child(8)::text')
+        loader.add_css('extension_options', 'p:nth-child(9)::text')
+        loader.add_css('lot_number', 'span.procgreentext::text')
+        loader.add_css('lot_description', 'span.procgreentext::text')
+        loader.add_css('lot_number', '.p:nth-child(12)::text')
+        loader.add_css('lot_description', 'p:nth-child(12)::text')
+        loader.add_css('lot_number', 'p:nth-child(10)>a::text')
+        loader.add_css('lot_description', 'p:nth-child(10)>a::text')
+        loader.add_xpath('supplier_name', '//td/p/a//text()')
+        yield loader.load_item()
